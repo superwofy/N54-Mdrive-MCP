2e315246c4668076e1ff0db2db29141270d7ff48
user0 <user0@mail.com>
2020-01-23 11:44:17 +0000

    Integrated Cory's library. Removed some unnecessary code for this application. Added traces. Still having some timeout issues.

--- N54-Mdrive-MCP.ino
+++ N54-Mdrive-MCP.ino
@@ -1,93 +1,106 @@
 //This sketch toggles sport mode in the IKM0S MSD81 flash in the absence of a DSCM90 or DSCM80 ZB.
-//Extra features: holding the M button toggles DTC on/off. 
+//Extra features: holding the M button toggles DTC on/off.
 
-#include <mcp_can.h>
+                                                    
+#define CAN0_INT 2                                  // INT. Adapt to your board
+#define DTC_CONTROL 1                               // Enable/Disable DTC toggling with M key
+
+#include "src/mcp_can.h"
+MCP_CAN CAN0(10);                                   // CS. Adapt to your board
 
 long unsigned int rxId;
-unsigned char len = 0, rxBuf[8];
-int mkey_pressed_counter = 0, i = 0x00;
-byte sndstat = CAN_OK, mkey_normal[] = {0xFF, 0x3F, 0x00}, mkey_pressed[] = {0xBF, 0x7F, 0x00}, dtc_pressed[] = {0xFD, 0xFF}, dtc_normal[] = {0xFC, 0xFF};
-bool ignition = false;
-
-MCP_CAN CAN0(10);                                // CS. Adapt to your board
-#define CAN0_INT 2                               // INT. Adapt to your board
+unsigned char rxBuf[8];
+int i = 0x00;
+byte sndstat, mkey_idle[] = {0xFF, 0x3F, 0x00}, mkey_pressed[] = {0xBF, 0x7F, 0x00};
+#if DTC_CONTROL
+byte dtc_pressed[] = {0xFD, 0xFF}, dtc_idle[] = {0xFC, 0xFF};
+int mkey_pressed_counter = 0;
+#endif
 
 void setup() {
-  Serial.begin(115200);
-  if(CAN0.begin(MCP_STDEXT, CAN_500KBPS, MCP_16MHZ) == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
-  else Serial.println("Error Initializing MCP2515.");
-
-  pinMode(CAN0_INT, INPUT);                             // Configuring pin for /INT input, pin 2
-
-  CAN0.init_Mask(0,0,0x07FFFFFF);                // Mask matches: 07FF (standard ID), first byte FF, second byte FF
-  CAN0.init_Mask(1,0,0x07FFFFFF);                // Mask matches: 07FF (standard ID), first byte FF, second byte FF
   
-  CAN0.init_Filt(0,0,0x01304550);                // Filter, ignition on
-  CAN0.init_Filt(1,0,0x01D6C00C);                // Filter, no MFL key pressed.
-  CAN0.init_Filt(2,0,0x01D6C04C);                // Filter, source key pressed
+#if DEBUG_MODE
+  Serial.begin(115200);
+  if(CAN0.begin() == CAN_OK) Serial.println("MCP2515 Initialized Successfully!");
+  else Serial.println("Error Initializing MCP2515.");
+#else
+  CAN0.begin();
+#endif
 
+  pinMode(CAN0_INT, INPUT);                     // Configuring pin for /INT input, pin 2
+
+  CAN0.init_Mask(0, 0x07FF0000);                // Mask matches: 07FF (standard ID) and all bytes
+  CAN0.init_Mask(1, 0x07FF0000);                // Mask matches: 07FF (standard ID) and all bytes
+  CAN0.init_Filt(0, 0x01D6C000);                // Filter MFL status.
   
   CAN0.setMode(MCP_NORMAL);
 }
 
-void loop() {
-  if (!digitalRead(CAN0_INT)) {                         // If pin 2 is low, read receive buffer
-    CAN0.readMsgBuf(&rxId, &len, rxBuf);                // Read data: len = data length, buf = data byte(s)
-
-    switch(rxId) {
-      case 0x130:                                       // Toggle ignition
-        if (rxBuf[0] == 0x45) ignition = true;
-        else ignition = false;
-      case 0x1D6:                                       // MFL actions
-        if (ignition) {
-          if (rxBuf[1] == 0x0C) {
-            send_mkeyidle();
-            mkey_pressed_counter = 0;
-          } else {
-            send_mkeypress();
-            mkey_pressed_counter++;
-            if (mkey_pressed_counter == 6) {
-              send_dtc();
-            }
-          }
-        }
-    }
+void loop() {   //Simply broadcast 1D9 as long as the PT-CAN is active.
+  if (!digitalRead(CAN0_INT)) {                   // If pin 2 is low, read receive buffer
+    CAN0.readMsgBuf(&rxId, rxBuf);                // Read data: rxId = CAN ID, buf = data byte(s)
+    if (rxBuf[0] == 0xC0 && rxBuf[1] == 0x4C) {
+      send_mkey_pressed();
+    #if DTC_CONTROL
+      if (mkey_pressed_counter == 6) {            // Should be about 6 * 200ms = 1.2 seconds
+        send_dtc_pressed();
+      }
+    #endif
+    } else send_mkey_idle();                      // If not C0 4C then send a ping.
   }
 }
 
-void send_mkeyidle() {
-  mkey_normal[2] = i;
-  sndstat = CAN0.sendMsgBuf(0x1D9, 0, 3, mkey_normal);  //no key pressed. Send every second to keep ECU happy. Timed with the MFL status.
+void send_mkey_idle() {
+  mkey_idle[2] = i;
+  sndstat = CAN0.sendMsgBuf(0x1D9, 3, mkey_idle);
+  
   while (sndstat != CAN_OK) {
+#if DEBUG_MODE
+    Serial.println(sndstat);
     Serial.println("Failed to send key idle message. Re-trying.");
-    sndstat = CAN0.sendMsgBuf(0x01D9, 0, 3, mkey_normal);
+#endif
+    sndstat = CAN0.sendMsgBuf(0x01D9, 3, mkey_idle);
   }
   i++;
   if (i == 0x100) i = 0x00;                             // i is used as the message counter
+#if DTC_CONTROL
+  mkey_pressed_counter = 0;
+#endif
 }
 
-void send_mkeypress() {
+void send_mkey_pressed() {
   mkey_pressed[2] = i;
-  sndstat = CAN0.sendMsgBuf(0x01D9, 0, 3, mkey_pressed);
+  sndstat = CAN0.sendMsgBuf(0x01D9, 3, mkey_pressed);
   while (sndstat != CAN_OK) {
+  #if DEBUG_MODE
     Serial.println("Failed to send key pressed message. Re-trying.");
-    sndstat = CAN0.sendMsgBuf(0x01D9, 0, 3, mkey_pressed);
+  #endif
+    sndstat = CAN0.sendMsgBuf(0x01D9, 3, mkey_pressed);
   }
   i++;
   if (i == 0x100) i = 0x00;
+#if DTC_CONTROL
+  mkey_pressed_counter++;
+#endif
+#if DEBUG_MODE
   Serial.println("Sent mkey press.");
+#endif
 }
 
-void send_dtc() {                                 // This exact sequence is required. If the timing is off, DSC module will ignore the message.
-  CAN0.sendMsgBuf(0x316, 0, 2, dtc_pressed);      // Adding enough (about 30) dtc_pressed will turn DSC off. Two are sent during a quick press of the button (DTC).
+#if DTC_CONTROL
+void send_dtc_pressed() {                         // This exact sequence is required. If the timing is off, DSC module will ignore the message.
+  CAN0.sendMsgBuf(0x316, 2, dtc_pressed);      // Adding enough (about 30) dtc_pressed will turn DSC off. Two are sent during a quick press of the button (DTC).
   delay(100);
-  CAN0.sendMsgBuf(0x316, 0, 2, dtc_pressed);
+  CAN0.sendMsgBuf(0x316, 2, dtc_pressed);
   delay(50);
-  CAN0.sendMsgBuf(0x316, 0, 2, dtc_normal);       // Three dtc_normal are always sent at the end 160ms apart as a sort of checksum.
+  CAN0.sendMsgBuf(0x316, 2, dtc_idle);         // Three dtc_idle are always sent at the end 160ms apart as a sort of checksum.
   delay(160);
-  CAN0.sendMsgBuf(0x316, 0, 2, dtc_normal);
+  CAN0.sendMsgBuf(0x316, 2, dtc_idle);
   delay(160);
-  CAN0.sendMsgBuf(0x316, 0, 2, dtc_normal);
+  CAN0.sendMsgBuf(0x316, 2, dtc_idle);
   mkey_pressed_counter = 0;
+  #if DEBUG_MODE
   Serial.println("Sent dtc key press.");
+  #endif
 }
+#endif
